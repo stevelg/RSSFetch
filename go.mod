2408ddbd907363218d73e1bd73392da631d66de1
Steven <sluong30@gatech.edu>
2024-05-07 14:53:29 -0700

    server up

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/stevelg/rssfetch
 
 go 1.22.1
+
+require (
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Starting server on port %s", portString)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("PORT: %s\n", portString)
 }
